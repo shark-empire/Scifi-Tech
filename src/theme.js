99377df26c898e4e5bff7ac256b8c2ed95fd89da
Shark-empire <babor3331@icloud.com>
2026-06-27 18:39:50 +0000

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const T = {
+  bg:        "#050A14",
+  bgCard:    "#080F1E",
+  bgCardHov: "#0C1526",
+  border:    "#0D1F3C",
+  borderGlow:"#00D4FF33",
+  cyan:      "#00D4FF",
+  cyanDim:   "#00A8CC",
+  violet:    "#7B2FFF",
+  violetDim: "#5E1FCC",
+  star:      "#E8F4FF",
+  muted:     "#5A7A9A",
+  text:      "#B8D4F0",
+};
